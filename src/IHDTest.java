e2d24e80efc650d1989be4a89353257030d60202
AndersNordman <seandnor@gmail.com>
2018-12-12 12:48:28 +0100

    push

--- src/IHDTest.java
+++ src/IHDTest.java
@@ -1,24 +1,25 @@
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.Test;
 
 class IHDTest {
-    private static IHD ihd; //
+    private static IHD ihd;
 
-    @BeforeAll
-    static void before(){
+    @BeforeEach
+    static void before() { 
         ihd = new IHD();
+        int a = 1;
     }
 
     @Test
     void startSensorTest() {
-        ihd.startSensor();
+        //ihd.startSensor();
         assertEquals(3, ihd.outputToSensor, "wrong voltage to sensor");
     }
 
     @Test
     void readSensorTest() {
-        ihd.readSensor();
+        //ihd.readSensor();
         assertEquals(0, ihd.outputToSensor, "wrong voltage to sensor");
     }
 }
