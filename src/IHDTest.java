3c6d98fd7910dec908956c3061bb1d2837415584
kanzh <kanzh@windows10.microdone.cn>
2018-12-12 13:54:53 +0100

    Gjort lite metoder public för att kunna köra testerna

--- src/IHDTest.java
+++ src/IHDTest.java
@@ -2,23 +2,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.Test;
 
-class IHDTest {
+public class IHDTest {
     private static IHD ihd;
 
     @BeforeEach
-    static void before() { 
+    public static void before() { 
         ihd = new IHD();
         int a = 1;
     }
 
     @Test
-    void startSensorTest() {
+    public void startSensorTest() {
         //ihd.startSensor();
         assertEquals(3, ihd.outputToSensor, "wrong voltage to sensor");
     }
 
     @Test
-    void readSensorTest() {
+    public void readSensorTest() {
         //ihd.readSensor();
         assertEquals(0, ihd.outputToSensor, "wrong voltage to sensor");
     }
