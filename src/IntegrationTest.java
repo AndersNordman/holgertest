942dd88726520a71487135f0fb5bc03973c0845a
AndersNordman <seandnor@gmail.com>
2018-12-18 12:53:15 +0100

    changed import

--- src/IntegrationTest.java
+++ src/IntegrationTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.mockito.Mockito.*;
 
 public class IntegrationTest {
